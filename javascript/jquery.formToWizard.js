72aafa31cc88a602a08b4adff8129c3b5c04ff9b
Harshal Shah <harshal.shah@gmail.com>
2011-01-10 16:00:37 +0530

    few more changes to accomodate new multi-step form

--- javascript/jquery.formToWizard.js
+++ javascript/jquery.formToWizard.js
@@ -3,20 +3,32 @@
 (function($) {
     $.fn.formToWizard = function(options) {
         options = $.extend({  
-            submitButton: "" ,
-            stepElements:"fieldset"
+            submitButton: "#submit" ,
+            stepElements:"fieldset",
+            next:"#next",
+            prev:"#prev",
+            navContainer:"#steps",
+            stepPrefix:"#step"
+
         }, options); 
         
         var element = this;
 
         var steps = $(element).find(options.stepElements);
         var count = steps.size();
-        var submmitButtonName = "#" + options.submitButton;
+        var submmitButtonName =  options.submitButton;
+        var prefix = options.stepPrefix;
         $(submmitButtonName).hide();
+		
+		var navContainer = options.navContainer;
 
         // 2
-        $(element).before("<ul id='steps'></ul>");
+        //$(element).before("<ul id='steps'></ul>");
 
+        steps.each(function(i) {
+
+        });
+/*
         steps.each(function(i) {
             $(this).wrap("<div id='step" + i + "'></div>");
             $(this).append("<p id='step" + i + "commands'></p>");
@@ -39,8 +51,8 @@
                 createNextButton(i);
             }
         });
-
-        function createPrevButton(i) {
+*/
+      function createPrevButton(i) {
             var stepName = "step" + i;
             $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Prev' class='prev'>< Back</a>");
 
@@ -54,7 +66,7 @@
 
         function createNextButton(i) {
             var stepName = "step" + i;
-            $("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next'>Next ></a>");
+            //$("#" + stepName + "commands").append("<a href='#' id='" + stepName + "Next' class='next'>Next ></a>");
 
             $("#" + stepName + "Next").bind("click", function(e) {
                 $("#" + stepName).hide();
@@ -66,9 +78,24 @@
         }
 
         function selectStep(i) {
-            $("#steps li").removeClass("current");
-            $("#stepDesc" + i).addClass("current");
-        }
+            $(navContainer).find("li").removeClass("active");
+            $(prefix + i).addClass("active");
 
+            $(prefix + i).next("li").addClass("upcoming");
+            $(prefix + i).prev("li").addClass("visited");
+        }
+		$(option.next).bind("click", function(e) {
+
+			// remove 'step' from 'step4' to get 4
+            var currentStep = $(navContainer).find("li.active").attr("id").replace("step","");
+            if (parseInt(currentStep) < (count -1) ) 
+            selectStep((parseInt(currentStep) + 1));
+        });
+        $(option.prev).bind("click", function(e) {
+
+            var currentStep = $(navContainer).find("li.active").attr("id").replace("step","");
+       		if (parseInt(currentStep) > 0) 
+       		selectStep((parseInt(currentStep) - 1));
+       });
     }
 })(jQuery); 
