e2c175d57512c2a8e139a9879a7c52b17211622a
Harshal Shah <harshal.shah@gmail.com>
2011-02-18 16:01:33 +0530

    task completed

--- javascript/controllers/task.js
+++ javascript/controllers/task.js
@@ -1,45 +1,78 @@
 TaskController = function(app) { with (app) {
 			
+			app.use("Template" , 'html');
 			app.before(/^#\/task-/, function(context) {
 
 				context.log("inisde task");
+				$("#main-content").html('');
+				$("#sidebar-content").html('');
+				$("#content-extra").html('');
 
 				// make sure the menu remains all the time
 
 		  		context.render('views/task-menu.html')
 		  			.replace("#section-menu")
-		  			.then(function(menu) {
-						$("#task-add").click(function() {
 
-							alert("comng");
+				context.render('views/task-add.html')
+					.replace("#sidebar-content")
+		  			.then(function(task_html) {
+					
+						$("#sidebar-content").hide();
+						$("#sidebar-content" ).find("input.datepicker").hide(); //datepicker( "option", "format", 'yyyy-mm-dd');
+						$("#section-menu").find("a.task-add").click(function() {
 
-							context.render('views/task-add.html')
-							.replace("#sidebar-content")
-							.then(function() {
-
-
-							});
+							$("#sidebar-content").toggle();
 
 							return false;
 						});
 		  			});
 			});
 
-			app.get('#/task-list', function(context) {
-			
+			bind("task-populate", function(ev, data) {
 				
+				this.render('views/task-list.html')
+					.replace("#main-content")
+					.then(function(html) {
+
+						this.renderEach("views/task-list-item.html", data)
+							.appendTo(".list > ul")
+							.then(function(html) {
 						
-				context.render('views/task-list.html')
-					.replace("#main-content");
+								$(".list :checkbox").click(function(ev) {
+									
+									$(this).parents("li:first").hide();
 
-
+									// make ajax call to database
+								});
+							});
+					});
 
 			});
 
-
-			
+			app.get('#/task-list', function(context) {
 				
+				context.redirect("#/task-pending");
+			});
+			app.get('#/task-pending', function(context) {
+						
+				context.load("data/tasks.json")
+				.then(function(json) {
+					context.trigger("task-populate", json);
+				});
 
+			});
+
+			app.post("#/task-add", function(context) {
+				
+				var form = context.params;
+				alert("coming");
+				$.post("/api/Task" , form, function(onSuccess) {
+
+					context.redirect("#/task-pending");
+				});
+
+				
+			});
 
 	}};
 
